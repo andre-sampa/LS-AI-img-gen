a3a4eb25cc2f4641dd294c3f9d635238ef4c5e4f
Andre <andrefranceschini@gmail.combitcoind>
2025-02-10 21:41:26 +0100

    “Update”

--- index.php
+++ index.php
@@ -126,7 +126,7 @@ function getImages($directory) {
                 grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
             }
         }
-    </style>aajhgkjgkjhg
+    </style>
 </head>
 <body>
     <div class="gallery-container">
