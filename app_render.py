20f0e245ef53d844dac659f91dedf40b25929cfb
Andre <andrefranceschini@gmail.combitcoind>
2025-02-14 18:23:49 +0100

    app render fix

--- app_render.py
+++ app_render.py
@@ -2,11 +2,12 @@
 import os
 from app_modal import demo
 # Debug: Print environment variables
-print("MODAL_TOKEN_ID:", os.getenv("MODAL_TOKEN_ID"))
-print("MODAL_TOKEN_SECRET:", os.getenv("MODAL_TOKEN_SECRET"))
-
-port = int(os.environ.get("PORT", 7860))  # Use Render's PORT or default to 7860
 token_id = os.getenv("MODAL_TOKEN_ID")
 token_secret = os.getenv("MODAL_TOKEN_SECRET")
+
+print(f"MODAL_TOKEN_ID: {token_id}")
+print(f"MODAL_TOKEN_SECRET: {token_secret}")
+
+port = int(os.environ.get("PORT", 7860))  # Use Render's PORT or default to 7860
 # Launch the Gradio app
 demo.launch(server_name="0.0.0.0", server_port=port)
