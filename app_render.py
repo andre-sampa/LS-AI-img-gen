671e28f61ed8b9952f5d6d466695ac9e0dc502b4
Andre <andrefranceschini@gmail.combitcoind>
2025-02-14 08:16:49 +0100

    add app_render.py

---
+++ app_render.py
@@ -0,0 +1,6 @@
+# Import gradio_interface
+import os
+from app_modal import demo
+port = int(os.environ.get("PORT", 10000))  # Use Render's PORT or default to 7860
+# Launch the Gradio app
+demo.launch(server_name="0.0.0.0", server_port=port)
