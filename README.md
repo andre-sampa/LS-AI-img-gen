16f92a80e648c71789baa299ee0298f103f5886b
Andre <andrefranceschini@gmail.combitcoind>
2025-02-15 03:18:59 +0100

    edit readme file to set gradio version

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ emoji: ⚔️
 colorFrom: green
 colorTo: green
 sdk: gradio
-sdk_version: 5.0.1
+sdk_version: 5.16.0
 app_file: app.py
 pinned: false
 ---
---
+++ app_render_inference.py
@@ -0,0 +1,13 @@
+# Import gradio_interface
+import os
+from app import demo
+# Debug: Print environment variables
+token_id = os.getenv("MODAL_TOKEN_ID")
+token_secret = os.getenv("MODAL_TOKEN_SECRET")
+
+print(f"MODAL_TOKEN_ID: {token_id}")
+print(f"MODAL_TOKEN_SECRET: {token_secret}")
+
+port = int(os.environ.get("PORT", 7860))  # Use Render's PORT or default to 7860
+# Launch the Gradio app
+demo.launch(server_name="0.0.0.0", server_port=port)
